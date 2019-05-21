045ae04973b627d5d4c66d374adc9a778092f376
Robski <chrzmichal@gmail.com>
2019-05-21 09:32:58 +0200

    Initial commit

---
+++ environment-mk/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\abc\desktop\environments\moja_komenda\environment-mk\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
