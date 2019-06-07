7b255ebc2934d48696f4a56ee9493d5e81ad7bc2
Robski <chrzmichal@gmail.com>
2019-06-07 15:55:12 +0200

    testing 07.06.2019

--- environment-mk/moja_komenda_blog_src/blog/views.py
+++ environment-mk/moja_komenda_blog_src/blog/views.py
@@ -1,26 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-posts =[
-    {
-        'author': 'Kamil Drukała',
-        'title': 'Rozprułem się po raz 1',
-        'content': 'Pamiętam to jak wczoraj..',
-        'date_posted': 'Listopad 29, 2019'
-    },
-    {
-        'author': 'Michał Chrząszcz',
-        'title': 'Nigdy się nie rozprułem',
-        'content': 'Pamiętam to jak wczoraj..',
-        'date_posted': 'Listopad 28, 2019'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'users': User.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
